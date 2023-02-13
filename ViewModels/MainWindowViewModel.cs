726bbb324903e9bd2540f4ecfddb7fbfac8486e6
Alexey Iskhakov <a.raiondesu@gmail.com>
2023-02-14 01:27:01 +0400

    Add initial avalonia app template

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Clicker.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Clicker.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
